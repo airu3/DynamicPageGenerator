713450052d4670130bd812833661e6641c6f141c
airu3 <sugar.tom5310@gmail.com>
2024-02-09 00:21:01 +0900

    MainApplication.javaの変更: ルートパスにindex.htmlを返すメソッドを追加

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MainApplication {
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
